7a69df4d2844ccbd5cb6058208c34b6c7615fe9f
yarcx <yakubuh56@gmail.com>
2023-11-13 12:51:20 +0100

    update on the card section

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Card from "../ui/dashboard/cards/card";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transaction from "../ui/dashboard/Transactions/Transaction";
+import Chart from "../ui/dashboard/Chart/Chart";
 
 const page = () => {
-  return <div>Dashboard page</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Chart />
+      </div>
+
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
 export default page;
